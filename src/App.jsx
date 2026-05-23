4c334240d9ca9e884e078774b134e87bb6cde7f8
ganeshkhadka337 <ganeshkhadka895@gmail.com>
2026-05-24 01:31:59 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
-return (
-  <Router>
-    <Routes>
-      <Route
-        path="/"
-        element={<PokemonList pokemonList={pokemonList} />}
-      />
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { useApi } from './useApi'
+import LoadingSpinner from './LoadingSpinner'
+import ErrorMessage from './ErrorMessage'
+import PokemonPage from './PokemonPage'
+import PokemonList from './PokemonList'
 
-      <Route
-        path="/pokemon/:name"
-        element={
-          <PokemonPage
-            pokemonList={pokemonList}
-            previous={previous}
-            next={next}
-          />
-        }
-      />
-    </Routes>
-  </Router>
-)
+const mapResults = ({ results }) => results.map(({ url, name }) => ({
+  url,
+  name,
+  id: Number(url.split('/').filter(Boolean).pop()),
+}))
+
+const App = () => {
+  const { data: pokemonList, error, isLoading } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=50', mapResults)
+
+  if (isLoading) return <LoadingSpinner />
+  if (error) return <ErrorMessage error={error} />
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<PokemonList pokemonList={pokemonList} />} />
+        <Route path="/pokemon/:name" element={<PokemonPage pokemonList={pokemonList} />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
